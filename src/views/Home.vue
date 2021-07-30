4085a62829d868c887e7543cb14f1800cc305e12
Christopher Kirkwood <christopherkirkwood@Christophers-MacBook-Air.local>
2021-07-29 19:27:25 -0700

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <div v-for="region in regions" v-bind:key="region.id"></div>
   </div>
 </template>
 
+<style></style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import regions from "https://pinballmap.com/api/v1/docs/1.0/regions/index.json";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      regions: regions.name,
+    };
+  },
+  created: function () {},
+  methods: {
+    //regions index
+    index: function () {
+      axios.get("https://pinballmap.com/api/v1/docs/1.0/regions/index.json").then((response) => {
+        console.log(regions);
+        console.log("regionsIndex", response);
+        this.regions = response.data;
+      });
+    },
   },
 };
 </script>
